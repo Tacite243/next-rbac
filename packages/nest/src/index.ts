a8252b100413e0fb8920bafd63e741e100f0991b
Tacite243 <taciteiragi@gmail.com>
2026-06-17 11:34:21 +0200

    feat(nest): initialization of NestJS integration

---
+++ packages/nest/src/index.ts
@@ -0,0 +1,7 @@
+export {
+    Authorize,
+    CurrentUser,
+    AuthorizeOptions,
+    NEXT_RBAC_METADATA_KEY
+} from './decorators.js';
+export { RbacGuard, NEXT_RBAC_ENGINE_TOKEN } from './rbac.guard.js';
